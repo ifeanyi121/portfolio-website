16ae88a3946d8fdfe42e6841c348e4fa90424d57
Ifeanyi Obi <obiify121@gmail.com>
2025-02-26 08:59:01 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,3 @@
-//
-// import './App.css'
-// import {useState,useEffect} from 'react'
-// const Card = ({title , actors}) => {
-//     const [count, setCount] = useState(0);
-//     const [hasLiked, setHasLiked] = useState(false);
-//     useEffect(() => {
-//         console.log(`${title} has ${hasLiked ? '' : 'not '}been liked`)
-//     })
-//   return (
-//       <div className= "card" >
-//         <h2>{title}</h2>
-//         <button onClick={ () => setCount(count+1) }>
-//             { hasLiked ? '❤️' : '🤍'}
-//         </button>
-//       </div>
-//   )
-// }
-// const App = () => {
-//
-//   return(
-//
-//       <div className="card-container">
-//         <Card title = "Star Wars" rating={5} isCool={true} actors = {['hope', ' and ','Faith', ]}/>
-//         <Card title="Avatar" />
-//         <Card title="The Lion King"/>
-//
-//
-//       </div>
-//
-//   )
-// }
-//
-// export default App
 
 
 import React from "react";
